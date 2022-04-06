e32ce11ef8f21183ce70bac189ecee2159e1a9d4
Eve <evestroud89@gmail.com>
2022-04-06 17:50:55 -0400

    Update answer for 04_removeFromArray
    Previous version of answer used filter instead of solving the problem
    myself with lower level code.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,13 @@
 const removeFromArray = function(array) {
-  for (i = 1; i < arguments.length; i ++) {
-    array = array.filter(item => item !== arguments[i]);
+  const newArray = [];
+  for (item of array) {
+    let match = false;
+    for (i = 1; i < arguments.length; i ++) {
+      if (item === arguments[i]) match = true;
+    }
+    if (!match) newArray.push(item);
   }
-  return array;
+  return newArray;
 };
 
 // Do not edit below this line
