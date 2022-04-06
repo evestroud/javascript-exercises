ec61ec82ddc3892ba142b80e7ceb5d78a3029d27
Eve <evestroud89@gmail.com>
2022-04-06 17:26:45 -0400

    Finish exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let split = string.split("");
+  let newString = "";
+  for (let i = split.length - 1; i >= 0; i--) {
+    newString += split[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
