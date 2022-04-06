c759d366132044ab26642b17ebbdaf5c122eeabf
Eve <evestroud89@gmail.com>
2022-04-06 17:59:21 -0400

    Add error handling for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  if (typeof x != "number" || typeof y != "number" || x < 0 || y < 0) {
+    return "ERROR";
+  }
+  let larger = (x >= y) ? x : y;
+  let smaller = (x < y) ? x : y;
 };
 
 // Do not edit below this line
